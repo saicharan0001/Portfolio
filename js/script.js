62e46c9b8c86a736d4fc3310a72a2acc718ca3df
saicharan <saicharan_b200970ec@nitc.ac.in>
2023-11-25 23:41:08 +0530

    Added Progressing bars for skills section

---
+++ js/script.js
@@ -0,0 +1,83 @@
+// DSA,STL,C++ progress circular bar 
+let htmlProgress = document.querySelector(".html-css"),
+  htmlValue = document.querySelector(".html-progress");
+
+let htmlStartValue = 0,
+  htmlEndValue = 80,
+  htmlspeed = 30;
+
+let progresshtml = setInterval(() => {
+  htmlStartValue++;
+
+  htmlValue.textContent = `${htmlStartValue}%`;
+  htmlProgress.style.background = `conic-gradient(#fca61f ${
+    htmlStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (htmlStartValue == htmlEndValue) {
+    clearInterval(progresshtml);
+  }
+}, htmlspeed);
+
+// javasript progress circular bar 
+let javascriptProgress = document.querySelector(".javascript"),
+  javascriptValue = document.querySelector(".javascript-progress");
+
+let javascriptStartValue = 0,
+  javascriptEndValue = 70,
+  jsspeed = 30;
+
+let progressjs = setInterval(() => {
+  javascriptStartValue++;
+
+  javascriptValue.textContent = `${javascriptStartValue}%`;
+  javascriptProgress.style.background = `conic-gradient(#7d2ae8 ${
+    javascriptStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (javascriptStartValue == javascriptEndValue) {
+    clearInterval(progressjs);
+  }
+}, jsspeed);
+
+// Nodejs and Express progress circular bar 
+let phpProgress = document.querySelector(".php"),
+  phpValue = document.querySelector(".php-progress");
+
+let phpStartValue = 0,
+  phpEndValue = 60,
+  phpspeed = 30;
+
+let progressphp = setInterval(() => {
+  phpStartValue++;
+
+  phpValue.textContent = `${phpStartValue}%`;
+  phpProgress.style.background = `conic-gradient(#20c997 ${
+    phpStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (phpStartValue == phpEndValue) {
+    clearInterval(progressphp);
+  }
+}, phpspeed);
+
+// sql progress circular bar 
+let reactProgress = document.querySelector(".reactjs"),
+  reactValue = document.querySelector(".reactjs-progress");
+
+let reactStartValue = 0,
+  reactEndValue = 75,
+  rjsspeed = 30;
+
+let progressreact = setInterval(() => {
+  reactStartValue++;
+
+  reactValue.textContent = `${reactStartValue}%`;
+  reactProgress.style.background = `conic-gradient(#3f396d ${
+    reactStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (reactStartValue == reactEndValue) {
+    clearInterval(progressreact);
+  }
+}, rjsspeed);
